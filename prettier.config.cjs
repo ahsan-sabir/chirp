3f90ce60cdfcd65032798f8c22cc450c554c89f0
ahsan-sabir <ahsan21916@gmail.com>
2023-08-15 22:28:24 +0500

    Add auth

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,17 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  // overrides: [
+  //   {
+  //     files: ['**/*.css', '**/*.scss'],
+  //     options: {
+  //       singleQuote: false,
+  //     },
+  //   },
+  // ],
+}
 
-module.exports = config;
+module.exports = config
