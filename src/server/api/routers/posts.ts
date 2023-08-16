9bdfa65e440d05464998f71afeb14eea8fb2f00d
ahsan-sabir <ahsan21916@gmail.com>
2023-08-17 00:00:01 +0500

    Profile page with ssg helper

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,21 +3,13 @@ import {
   privateProcedure,
   publicProcedure,
 } from '~/server/api/trpc'
-import { clerkClient } from '@clerk/nextjs'
-import type { User } from '@clerk/nextjs/dist/types/server'
+import { clerkClient } from '@clerk/nextjs/server'
 import { TRPCError } from '@trpc/server'
 import { z } from 'zod'
 
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  }
-}
-
 import { Ratelimit } from '@upstash/ratelimit' // for deno: see above
 import { Redis } from '@upstash/redis'
+import { filterUserForClient } from '~/server/helpers/filterUserForClient'
 
 // Create a new ratelimiter, that allows 10 requests per 10 seconds
 const ratelimit = new Ratelimit({
